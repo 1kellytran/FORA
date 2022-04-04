c87d1651c876bd0e378a5798b12a22b4856c6b46
alexurtti <92301547+alexurtti@users.noreply.github.com>
2022-04-04 16:17:39 +0200

    added properties in DTOModel

--- Fora/Shared/UserDtoModel.cs
+++ Fora/Shared/UserDtoModel.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace Fora.Shared
 {
-    public class UserDtoModel
+    internal class UserDTO
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
     }
 }
