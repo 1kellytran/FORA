4d7f880ee7f7b37b4e713d62ba7145d053d1285c
alexurtti <92301547+alexurtti@users.noreply.github.com>
2022-04-04 16:06:01 +0200

    UserController SigninManager fixes

---
+++ Fora/Shared/UserDTOModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared
+{
+    internal class UserDTO
+    {
+    }
+}
