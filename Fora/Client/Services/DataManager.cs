0970bc1aedafbb2651dc2e6e626d5b2a1f3e90b0
1kellytran <kellytran96@hotmail.com>
2022-04-11 12:13:41 +0200

    ThreadStart

--- Fora/Client/Services/DataManager.cs
+++ Fora/Client/Services/DataManager.cs
@@ -1,23 +1,15 @@
 ﻿using Fora.Shared;
 using System.Net.Http.Json;
 
-
-
-
 namespace Fora.Client.Services
 {
     public class DataManager : IDataManager
     {
         private readonly HttpClient _httpClient;
-
-
-
         public DataManager(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
-
-
 
         public async Task CreateInterest(InterestModel interestToAdd)
         {
@@ -25,27 +17,18 @@ namespace Fora.Client.Services
 
         }
 
-
-
         public async Task<List<InterestModel>> GetAllInterests()
         {
             List<InterestModel> interests = new();
             interests = await _httpClient.GetFromJsonAsync<List<InterestModel>>("api/interest");
 
             return interests;
-
         }
-
-
 
         public async Task<string> DeleteInterest(int id)
         {
             var result = await _httpClient.DeleteAsync("api/intesrest");
             return result.ToString(); //är detta strängen message?
         }
-
-
-
-
     }
 }
---
+++ Fora/Server/Controllers/ThreadController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Fora.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThreadController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ThreadController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ThreadController>
+        [HttpGet]
+        public async Task<List<ThreadModel>> GetAllThreads(int id)
+        {
+            InterestModel interest = new();
+            ThreadModel threads = new();
+
+            threads = _context.Threads.Where(t => t.Id == id).ToList();
+            
+            interest = _context.Interests.FirstOrDefault(i => i.Id == id);
+            return _context.Threads.ToList();
+        }
+
+        // GET api/<ThreadController>/5
+        [HttpGet("{id}")]
+        public async Task<List<ThreadModel>> GetThreadByID(int id)
+        {
+            return _context.Threads.FirstOrDefault(t => t.InterestId == id).ToList();            
+        }
+
+        // POST api/<ThreadController>
+        [HttpPost]
+        public async Task CreateThread([FromBody]ThreadModel threadToAdd)
+        {
+            // Get InterestID 
+
+            // Set InterestID for ThreadModel while creating Thread
+            InterestModel interest = new();
+            ThreadModel thread = new();
+
+            interest = _context.Interests.FirstOrDefault(i => i.Id == threadToAdd.InterestId);
+
+        }
+
+        // PUT api/<ThreadController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ThreadController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
